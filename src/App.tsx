82caf60686365df5e1d3cf269770634b158e6ff8
wooodiest <michalkuchnickiisc@gmail.com>
2025-08-26 14:59:50 +0200

    Added Tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Profile from './Components/Profile';
 export default function App() {
   return (
     <div className="app">
+      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind + Electron!</h1>
       <Profile />
     </div>
   );
